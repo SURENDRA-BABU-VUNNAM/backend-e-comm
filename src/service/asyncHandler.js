2058a4ed2d1332a5765d2af21336764a0dec6dd7
SURENDRA-BABU-VUNNAM <83397060+SURENDRA-BABU-VUNNAM@users.noreply.github.com>
2023-04-17 05:54:27 +0000

    async higher order function for controler

---
+++ src/service/asyncHandler.js
@@ -0,0 +1,13 @@
+
+const asyncHandler = (fn) => async (res,req,next) =>{
+    try {
+       await fn(res,req,next) 
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message:error.message,
+        })
+    }
+}
+
+export default asyncHandler
